74bbce1efa52a73677b9aaacd8d4eaa125aa9a71
cameronmgj <123835556+cameronmgj@users.noreply.github.com>
2024-06-27 13:05:45 +0100

    Create test0.plugin.js

---
+++ test0.plugin.js
@@ -0,0 +1,17 @@
+/**
+ * @name BD test 0
+ * @author cameronmgj
+ * @version 0.0.1
+ * @description test console.log
+ *
+ * @invite XXXXXXXXXX
+ * @website https://github.com
+ * @source https://github.com
+ * @updateUrl https://raw.githubusercontent.com
+ */
+
+start() {
+		try {
+			console.log("Hello World!")
+		}
+	}
