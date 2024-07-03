dadafbfd937f0bcca119b9d6a1cf954ff7bac1d3
cameronmgj <123835556+cameronmgj@users.noreply.github.com>
2024-07-03 15:40:08 +0100

    completely functional
    added settings. commented out debug log to reduce resource utilization, testing

--- test0.plugin.js
+++ test0.plugin.js
@@ -1,55 +1,138 @@
 /**
- * @name BD Hide Shop
- * @author cameronmgj
- * @description no more bloat - hides bd shop tab from main page
- * @version 0.1.0
+ * @name TutorialPlugin2
+ * @author YourName
+ * @description Learning how to make BetterDiscord plugins!
+ * @version 0.0.1
  */
-module.exports = class MyPlugin {
-	constructor(meta) {
-        // Initialization code
-		this.initObserver();
-	};
+
+ module.exports = meta => {
+
+    const mySettings = {friends: true, nitro: true, shop: true};
+
+    function buildSetting(text, key, type, value, callback = () => {}) {
+        const setting = Object.assign(document.createElement("div"), {className: "setting"});
+        const label = Object.assign(document.createElement("span"), {textContent: text});
+        const input = Object.assign(document.createElement("input"), {type: type, name: key, value: value});
+        if (type == "checkbox" && value) input.checked = true;
+        input.addEventListener("change", () => {
+            const newValue = type == "checkbox" ? input.checked : input.value;
+            mySettings[key] = newValue;
+            BdApi.Data.save(meta.name, "settings", mySettings);
+            callback(newValue);
+        });
+        setting.append(label, input);
+        return setting;
+    }
+
 	
-	//track changes to page
-	initObserver() {
+	function initObserver() {
         const observer = new MutationObserver((mutationsList) => {
             for (const mutation of mutationsList) {
                 if (mutation.type === 'childList') {
                     // actions to take
-                    this.run();
+                    run();
                 }
             }
 		});
-		const targetNode = document.body; // You can specify a different target node if needed
+		const targetNode = document.querySelector('.appAsidePanelWrapper_bd26cc');
 		const config = { childList: true, subtree: true };
         observer.observe(targetNode, config);
 	};
 	
 	//run when called
-	run() {
-        console.log("--hiding-shop--");
-		this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
-		const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
-		if (DMsIndex > 2) {
-			this.shopElements[2].style.display = "none";
+	function run() {
+		if (mySettings.friends) {
+			if (mySettings.nitro) {
+				if (mySettings.shop) {
+					/* console.log("--hiding-friends--");
+					console.log("--hiding-nitro--");
+					console.log("--hiding-shop--"); */
+					this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+					const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+					if (DMsIndex > 0) {
+						this.shopElements[2].style.display = "none";
+						this.shopElements[1].style.display = "none";
+						this.shopElements[0].style.display = "none";
+					}
+				}
+				else {
+					/* console.log("--hiding-friends--");
+					console.log("--hiding-nitro--"); */
+					this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+					const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+					if (DMsIndex > 1) {
+						this.shopElements[1].style.display = "none";
+						this.shopElements[0].style.display = "none";
+					}
+				}
+			}
+			else if (mySettings.shop) {
+				/* console.log("--hiding-friends--");
+				console.log("--hiding-shop--"); */
+				this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+				const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+				if (DMsIndex > 1) {
+					this.shopElements[2].style.display = "none";
+					this.shopElements[0].style.display = "none";
+				}
+			}
+			else {
+				/* console.log("--hiding-friends--"); */
+				this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+				const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+				if (DMsIndex > 2) {
+					this.shopElements[0].style.display = "none";
+				}
+			}
 		}
-		
-    }
+		else if (mySettings.nitro) {
+			if (mySettings.shop) {
+				/* console.log("--hiding-nitro--");
+				console.log("--hiding-shop--"); */
+				this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+				const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+				if (DMsIndex > 1) {
+					this.shopElements[2].style.display = "none";
+					this.shopElements[1].style.display = "none";
+				}
+			}
+			else {
+				/* console.log("--hiding-nitro--"); */
+				this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+				const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+				if (DMsIndex > 2) {
+					this.shopElements[2].style.display = "none";
+				}
+			}
+		}
+		else if (mySettings.shop) {
+			/* console.log("--hiding-shop--"); */
+			this.shopElements = document.querySelectorAll('[role="listitem"].channel_c91bad.container_b15955, .privateChannelsHeaderContainer_c47fa9');
+			const DMsIndex = Array.from(this.shopElements).findIndex(element => element.classList.contains("privateChannelsHeaderContainer_c47fa9"));
+			if (DMsIndex > 2) {
+				this.shopElements[2].style.display = "none";
+			}
+		}
+    };
 
-	//run on startup
-	start() {
-		// Code to run when enabled
-	};
-	
-	//run at end
-    stop() {
-        // Cleanup when disabled
-    }	
+  return {
+    start: () => {
+        Object.assign(mySettings, BdApi.Data.load(meta.name, "settings"));
+		run();
+		initObserver();
+    },
+    stop: () => {
+    },
+    getSettingsPanel: () => {
+        const mySettingsPanel = document.createElement("div");
+        mySettingsPanel.id = "my-settings";
+
+        const friends = buildSetting("Friends", "friends", "checkbox", mySettings.friends, run);
+		const nitro = buildSetting("Nitro", "nitro", "checkbox", mySettings.nitro, run);
+        const shop = buildSetting("Shop", "shop", "checkbox", mySettings.shop, run);
+
+        mySettingsPanel.append(friends, nitro, shop);
+        return mySettingsPanel;
+    }
+  }
 };
-	
-	
-/* to do:
-	- add settings
-	- remove nitro tab
-	- remove friends tab
-*/
